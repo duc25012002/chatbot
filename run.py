12b91b5dbe9d69e43801e6017ffcf30bff04d242
Hoàng Đức <ductrieuhoang@gmail.com>
2025-03-13 14:03:19 +0700

    Update

--- run.py
+++ run.py
@@ -61,4 +61,4 @@ def ask_question():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000)) 
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
