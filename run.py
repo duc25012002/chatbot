f2dcc46a3a9dcfe0154cf9540baac9b36d4bb1f5
Hoàng Đức <ductrieuhoang@gmail.com>
2025-03-13 14:14:54 +0700

    Update

--- run.py
+++ run.py
@@ -60,5 +60,5 @@ def ask_question():
     return jsonify({"question": question, "answer": answer})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000)) 
+    port = int(os.environ.get("PORT", 8000)) 
     app.run(debug=True, host="0.0.0.0", port=port)
