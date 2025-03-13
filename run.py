c13d3cae57dface43c55c7be432e0b46f3666be2
Hoàng Đức <ductrieuhoang@gmail.com>
2025-03-13 15:29:29 +0700

    Update

--- run.py
+++ run.py
@@ -48,8 +48,8 @@ def ask_question():
     print(question)
     # Lấy thông tin context từ Pinecone
 
-    
-    vector_store = Pinecone(index=index, embedding=embeddings)
+
+    vector_store = Pinecone.from_existing_index(index=index, embedding=embeddings)
     result = vector_store.similarity_search(question)
 
     context = " ".join([doc.page_content for doc in result])
