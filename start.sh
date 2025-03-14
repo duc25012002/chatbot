f6589ba066ec2ad746075235c1d2cde8ad399124
Hoàng Đức <ductrieuhoang@gmail.com>
2025-03-14 10:18:35 +0700

    dá

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 chmod +x start.sh
-rm -rf /opt/render/.local/lib/python*
 pip install --no-cache-dir -r requirements.txt
-pip install gunicorn
-pip install flask
-export PATH=$PATH:/opt/render/.local/bin
 gunicorn -w 4 -b 0.0.0.0:$PORT run:app  
