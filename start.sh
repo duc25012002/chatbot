873e83f4ab97f98d29448c75877e3a33ca83226a
Hoàng Đức <ductrieuhoang@gmail.com>
2025-03-14 09:53:13 +0700

    Update

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-echo '#!/bin/bash' > start.sh
-echo 'gunicorn -w 4 -b 0.0.0.0:$PORT run:app' >> start.sh
-chmod +x start.sh  # Cấp quyền thực thi
+#!/bin/bash
+chmod +x start.sh
+pip install --no-cache-dir -r requirements.txt  
+gunicorn -w 4 -b 0.0.0.0:$PORT run:app  
